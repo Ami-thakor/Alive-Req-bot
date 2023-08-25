268e55ec733f7503041a84adaffe86a4ec4bf328
crazebotz <120011436+crazebotz@users.noreply.github.com>
2023-08-25 08:27:19 +0530

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,20 +1,21 @@
 import os
 import motor.motor_asyncio
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://admin:rahul@myusers.qsyvptv.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get(
+    "DB_URL", "mongodb+srv://admin:rahul@myusers.qsyvptv.mongodb.net/?retryWrites=true&w=majority")
 myclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URL)
 mydb = myclient["KingReqBot"]
 mycol = mydb["users"]
 users_collection = mydb["users"]
 
+
 async def add_user(message):
     try:
         userDATA = {"_id": message.id, "name": message.first_name}
         await mycol.insert_one(userDATA)
-    except motor.motor_asyncio.DuplicateKeyError:
+    except:
         pass
-    except Exception as ex:
-        print(ex)
+
 
 async def getid():
     values = []
